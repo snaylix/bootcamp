26a18ff5603476ef2123d7b47743dad2ac18cf82
snay <feligges@gmx.de>
2020-08-20 17:21:48 +0200

    fixed mistakes in dashboard script

--- 06_week/dashboard.sh
+++ 06_week/dashboard.sh
@@ -1,16 +1,13 @@
 # Download Metabase.jar with wget
-wget wget https://downloads.metabase.com/v0.35.3/metabase.jar
+wget https://downloads.metabase.com/v0.35.3/metabase.jar
 
 # We need Java to launch the application
 # Download Java directly from the Amazon Linux Repository
 sudo yum install java-1.8.0
 
-# Launch it
-java -jar metabase.jar
-
-# We need to make sure that when we log out / disconnect from the AWS machine, the Metabase server is still running in the background.
-screen
-# Screen, or GNU Screen, is a terminal multiplexer. In other words, it means that you can start a screen ‘session’ and then open any number of windows (virtual terminals) inside that session. Processes running in Screen will continue to run when their window is not visible even if you get disconnected.
+# Kill everything that tries to access port 80, to make sure that it's free
+sudo kill -9 $(sudo lsof -t -i:80)
 
 # This will launch a Metabase server on port 3000 by default. We need to redirect default Metabase port (:3000) to the public HTTP port (:80)
-sudo java -jar -DMB_JETTY_PORT=80 metabase.jar
+# We need to make sure that when we log out / disconnect from the AWS machine, the Metabase server is still running in the background.
+sudo nohup java -jar -DMB_JETTY_PORT=80 metabase.jar &
